8a868b0a50d237364caf133e67292611ac9301b2
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-05-05 21:30:55 +0800

    adding login route

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const server = app.listen(port, () => {
 app.set("view engine", "pug");
 app.set("views", "views");
 
+//routes
+const loginRoute = require("./routes/loginRoutes");
+
+app.use("/login", loginRoute);
+
 app.get("/", middleware.requireLogin, (req, res, next) => {
   var payload = {
     pageTitle: "Home",
